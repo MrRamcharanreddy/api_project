5d2acffcd4e447a61d068fc02356692775cce0ca
MrRamcharanreddy <126326179+MrRamcharanreddy@users.noreply.github.com>
2023-06-27 15:34:20 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class APITestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_total_items(self):
+        response = self.client.get('/api/total_items/')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIn('total_items', data)
+
+        # Add more assertions to validate the response data and behavior
+
+    def test_nth_most_total_item(self):
+        response = self.client.get('/api/nth_most_total_item/')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIn('nth_most_total_item', data)
+
+        # Add more assertions to validate the response data and behavior
+
+    def test_percentage_of_department_wise_sold_items(self):
+        response = self.client.get('/api/percentage_of_department_wise_sold_items/')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIn('percentage_of_department_wise_sold_items', data)
+
+        # Add more assertions to validate the response data and behavior
+
+    def test_monthly_sales(self):
+        response = self.client.get('/api/monthly_sales/')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIn('monthly_sales', data)
+
+        # Add more assertions to validate the response data and behavior
