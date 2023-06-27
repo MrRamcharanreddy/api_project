b89179deb40f04eedf6787b25d7a4f7aaed4da38
MrRamcharanreddy <126326179+MrRamcharanreddy@users.noreply.github.com>
2023-06-27 15:32:08 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from api_app import views
 
 urlpatterns = [
+    path('', views.home, name='home'),  # Add this line for the root URL
     path('api/total_items/', views.total_items, name='total_items'),
     path('api/nth_most_total_item/', views.nth_most_total_item, name='nth_most_total_item'),
     path('api/percentage_of_department_wise_sold_items/', views.percentage_of_department_wise_sold_items, name='percentage_of_department_wise_sold_items'),
